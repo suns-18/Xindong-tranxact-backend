ea3a58ec15e67a0e6b2e1288dab6eea9b4880e59
NBDatsuya <2646809050@qq.com>
2024-04-14 01:35:08 +0800

    init commit

---
+++ src/main/java/neu/xindong/xact/XindongTranxactBackendApplication.java
@@ -0,0 +1,13 @@
+package neu.xindong.xact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XindongTranxactBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XindongTranxactBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/neu/xindong/xact/XindongTranxactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package neu.xindong.xact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XindongTranxactBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
