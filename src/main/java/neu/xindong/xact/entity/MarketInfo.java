9554930874850c9dba88bc5ba3ecdc25e8c8d8ad
NBDatsuya <2646809050@qq.com>
2024-04-18 04:41:37 +0800

    fix: MarketInfo entity constructor support

--- src/main/java/neu/xindong/xact/entity/MarketInfo.java
+++ src/main/java/neu/xindong/xact/entity/MarketInfo.java
@@ -1,13 +1,19 @@
 package neu.xindong.xact.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.util.Date;
 import org.springframework.data.redis.core.index.Indexed;
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @RedisHash("market_info")
+@SuperBuilder
 public class MarketInfo {
     @Id
     private String stockCode;
@@ -18,5 +24,4 @@ public class MarketInfo {
     private Double yesterdayCollectionPrice;
     private Double limitUpPrice;
     private Double limitDownPrice;
-
 }
