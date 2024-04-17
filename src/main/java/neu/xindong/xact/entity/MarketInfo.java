78e558cebee32087c5a6eb5e0074cdcebd54fb51
NBDatsuya <2646809050@qq.com>
2024-04-18 03:17:11 +0800

    feat: MarketInfo entity using redis

--- src/main/java/neu/xindong/xact/entity/MarketInfo.java
+++ src/main/java/neu/xindong/xact/entity/MarketInfo.java
@@ -1,15 +1,22 @@
 package neu.xindong.xact.entity;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.util.Date;
-
+import org.springframework.data.redis.core.index.Indexed;
 @Data
+@RedisHash("market_info")
 public class MarketInfo {
+    @Id
+    private String stockCode;
+
+    @Indexed
+    private Date marketTime;
     private Double currentPrice;
     private Double yesterdayCollectionPrice;
-    private String stockCode;
     private Double limitUpPrice;
     private Double limitDownPrice;
-    private Date marketTime;
+
 }
