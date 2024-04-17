b0c01e6c62514f1fa5d40956ccb27ee9dd4f6d39
NBDatsuya <2646809050@qq.com>
2024-04-18 04:41:19 +0800

    fix: MarketInfoRepo pageable support

--- src/main/java/neu/xindong/xact/service/MarketInfoRepository.java
+++ src/main/java/neu/xindong/xact/service/MarketInfoRepository.java
@@ -2,11 +2,15 @@ package neu.xindong.xact.service;
 
 import neu.xindong.xact.entity.MarketInfo;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.Optional;
 
+@Service
 public interface MarketInfoRepository extends CrudRepository<MarketInfo, String> {
     @Override
     <S extends MarketInfo> S save(S entity);
@@ -15,10 +19,9 @@ public interface MarketInfoRepository extends CrudRepository<MarketInfo, String>
 
     boolean existsByStockCode(String s);
 
-    Page<MarketInfo> findMarketInfosByMarketTime(Date marketTime);
+    Page<MarketInfo> findMarketInfosByMarketTime(Date marketTime, Pageable page);
 
-    @Override
-    Page<MarketInfo> findAll();
+    Page<MarketInfo> findAll(Pageable page);
 
     @Override
     long count();
