0526f6b409501c3dcae24fe6d0d7465b7d82d71e
NBDatsuya <2646809050@qq.com>
2024-04-18 03:17:26 +0800

    feat: MarketInfoResp

---
+++ src/main/java/neu/xindong/xact/dto/response/MarketInfoResp.java
@@ -0,0 +1,10 @@
+package neu.xindong.xact.dto.response;
+
+import neu.xindong.xact.entity.MarketInfo;
+
+import java.util.List;
+
+public class MarketInfoResp {
+    List<MarketInfo> list;
+    String msg;
+}
