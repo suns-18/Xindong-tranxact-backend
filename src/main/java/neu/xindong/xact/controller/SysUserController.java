4940c13f8ecc4782439d3f87dcf961bdddc26b3b
NBDatsuya <2646809050@qq.com>
2024-04-19 14:54:11 +0800

    fix：Controller name & SysUserAuthorize.id

--- src/main/java/neu/xindong/xact/controller/SysUserController.java
+++ src/main/java/neu/xindong/xact/controller/SysUserController.java
@@ -1,14 +1,13 @@
 package neu.xindong.xact.controller;
 
 import neu.xindong.xact.dto.HttpResponse;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/sysUser")
-public class SysUserCotroller {
+public class SysUserController {
     @GetMapping("/test")
     public HttpResponse<String> a(){
         return HttpResponse.success("okkkk");
