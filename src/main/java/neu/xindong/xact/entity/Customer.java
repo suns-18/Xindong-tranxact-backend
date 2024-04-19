b8b8589c1df829352d7cb5703563799034c7f4fd
wangwenqingqq <1064078316@qq.com>
2024-04-19 22:44:30 +0800

    fix : Customer:随机生成客户代码

--- src/main/java/neu/xindong/xact/entity/Customer.java
+++ src/main/java/neu/xindong/xact/entity/Customer.java
@@ -2,18 +2,33 @@ package neu.xindong.xact.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+@SuperBuilder
 @Data
 @TableName("customer")
 @ToString
 public class Customer {
+
     @TableId
-    private Integer id;
+    @Builder.Default
+    private Integer id= generateRandomId();
     private String customerName;
     private String idType;
     private String idNumber;
     private Integer cuacctCls;
     private Integer cuacctStatus;
+
+    // 工具方法：生成一个固定8位的随机数字ID，不足8位时补零
+    private static Integer generateRandomId() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(90000000); // 生成0至89999999之间的随机数
+        DecimalFormat decimalFormat = new DecimalFormat("00000000");//不足8位的前面补0
+        return Integer.parseInt(decimalFormat.format(randomNumber));
+    }
 }
