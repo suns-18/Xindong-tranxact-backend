25632d6c923d6b9722261c2160a25a5234d8c07f
wangwenqingqq <1064078316@qq.com>
2024-04-25 12:39:28 +0800

    feat : generateCustomerId

--- src/main/java/neu/xindong/xact/controller/CustomerController.java
+++ src/main/java/neu/xindong/xact/controller/CustomerController.java
@@ -13,11 +13,12 @@ import neu.xindong.xact.service.PrimeAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static neu.xindong.xact.util.RegisterUtil.CreateCustomerId;
-import static neu.xindong.xact.util.RegisterUtil.CreateFollowAccount;
+
+import static neu.xindong.xact.util.RegisterUtil.createFollowAccount;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -64,7 +65,23 @@ public class CustomerController {
             return HttpResponse.failureWhenAccessDB();
         }
     }
-
+    @GetMapping("/generateCustomerId")
+    @Operation(summary = "生成客户代码",
+            description = "返回客户代码")
+    public HttpResponse<Integer> generateCustomerId(){
+        try {
+            var lastCustomer = customerService.findLastCustomerOrderById();
+            if(lastCustomer==null) {
+                //10000001 ->lastCustomer;
+                return HttpResponse.success(44444444);
+            }
+            else{
+                return HttpResponse.success(lastCustomer.getId()+1);
+            }
+        } catch (Exception e) {
+            return HttpResponse.failureWhenAccessDB();
+        }
+    }
     @PostMapping("/save")
     @Operation(summary = "保存客户",
             description = "输入客户的信息，提交到服务器，返回提交操作的结果")
@@ -75,7 +92,7 @@ public class CustomerController {
 
             // 使用AccountRegisterReq对象的数据创建Customer对象
             Customer customer = Customer.builder()
-                    .id(CreateCustomerId())
+                    .id(accountRegisterRequest.getCustomer().getId())
                     .customerName(accountRegisterRequest.getCustomer().getCustomerName())
                     .idType(accountRegisterRequest.getCustomer().getIdType())
                     .idNumber(accountRegisterRequest.getCustomer().getIdNumber())
@@ -87,12 +104,16 @@ public class CustomerController {
                     .balanceTotal(100000.0)//现在只是统一存入100000元
                     .build();
             // 使用流式编程创建 FollowAccount 列表
-            List<FollowAccount> followAccounts = accountRegisterRequest.getFollowAccountList().stream()
-                    .map(followAccount -> FollowAccount.builder()
-                            .primeAccountId(customer.getId())
-                            .market(followAccount.getMarket())
-                            .updateTime(followAccount.getUpdateTime())
-                            .id(CreateFollowAccount(followAccount.getMarket(), customer.getCuacctCls()))
+            List<FollowAccount> followAccounts = accountRegisterRequest.getMarket().stream()
+                    .map(market -> FollowAccount.builder()
+                            .primeAccountId(customer.getId()) // 假设这里需要客户ID作为主账户ID
+                            .market(market)
+                            .updateTime(new Date()) // 设置当前时间为更新时间
+                            // 假设id是自动生成的
+                            .id(createFollowAccount(market,customer.getCuacctCls()))
+                            // balanceTotal和balanceUsable在这里是useless，根据实际情况设置
+                            .balanceTotal(0.0)
+                            .balanceUsable(0.0)
                             .build())
                     .collect(Collectors.toList());
             followAccountService.saveBatch(followAccounts);
