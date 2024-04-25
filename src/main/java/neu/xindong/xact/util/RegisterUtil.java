88516d13d311a4395d4802432b9587d209b2acab
wangwenqingqq <1064078316@qq.com>
2024-04-25 12:40:35 +0800

    fix : RegisterUtil

--- src/main/java/neu/xindong/xact/util/RegisterUtil.java
+++ src/main/java/neu/xindong/xact/util/RegisterUtil.java
@@ -4,21 +4,21 @@ import java.util.Random;
 
 public class RegisterUtil {
     // 工具方法：生成客户代码：一个固定8位的随机数字ID，不足8位时补零
-    public static Integer CreateCustomerId(){
+    public static Integer createCustomerId(){
         Random random = new Random();
         int randomNumber = random.nextInt(90000000); // 生成0至89999999之间的随机数
         DecimalFormat decimalFormat = new DecimalFormat("00000000");//不足8位的前面补0
         return Integer.parseInt(decimalFormat.format(randomNumber));
     }
     // 工具方法：生成客户代码：一个固定8位的随机数字ID，不足8位时补零
-    public static Integer CreateOrderId(){
+    public static Integer createOrderId(){
         Random random = new Random();
         int randomNumber = random.nextInt(9000000); // 生成0至8999999之间的随机数
         DecimalFormat decimalFormat = new DecimalFormat("0000000");//不足7位的前面补0
         return Integer.parseInt(decimalFormat.format(randomNumber));
     }
     //工具方法：生成从账户：一个固定10位的随机字符串ID
-    public static String CreateFollowAccount(int market , int cls){
+    public static String createFollowAccount(int market , int cls){
         String account ;
         Random random = new Random();
         // 生成一个9位的随机数字
